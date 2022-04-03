1f703446fd483e439d1db1699968b8666373368e
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-04-03 15:43:29 -0400

    fixed card font color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,10 +6,8 @@ const Card = (props) => {
             <div className="card-header">
                 {props.description}
             </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">{props.date}</li>
-                <li className="list-group-item">${props.amount}</li>
-            </ul>
+            <p>{props.date}</p>
+            <p>${props.amount}</p>
         </div>
     );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Debits from './Debits';
 import Credits from './Credits';
-import Customize from './Customize';
 
 const Content = () => {
     const [page, setPage] = useState(<Debits />);
