e29ec93c51f7ce92bacb5b3f67b411ea507a25ad
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-04-03 13:24:14 -0400

    customize now closes when updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -15,9 +15,37 @@ const Content = () => {
     }
 
     const showCustomizeProfile = () => {
-        setPage(<Customize />)
+        setPage('Customize')
     }
 
+    const update = () => {
+        let username = document.getElementById('username-textbox').value;
+        if (username !== '') {
+            document.getElementById("username").innerHTML = username;
+        }
+        let color = document.getElementById('color-textbox').value;
+        if (color !== '') {
+            document.body.style.backgroundColor = color;
+        }
+        setPage(<Debits />)
+    }
+
+    if (page === 'Customize') {
+        return (
+            <div className="container">
+                <button className='btn btn-primary' onClick={showDebits}>Debits</button>
+                <button className='btn btn-primary' onClick={showCredits}>Credits</button>
+                <button className='btn btn-primary' onClick={showCustomizeProfile}>Customize</button>
+                <h1>Customize</h1>
+                <br />
+                <form id="color-form">
+                    Username: <input type="text" name="username" id='username-textbox' /><br />
+                    Color: <input type="text" name="color" id='color-textbox' /><br />
+                </form>
+                <button className='btn btn-outline-primary' onClick={update}>Update</button>
+            </div>
+        );
+    }
     return (
         <div>
             <button className='btn btn-primary' onClick={showDebits}>Debits</button>
